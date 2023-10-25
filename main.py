68949185a506ff6990866cd269142e897088ec39
JananiiM21 <jananiim21@gmail.com>
2023-10-25 14:39:35 -0400

    Added MySQL Connector code

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import numpy as np
 import pandas as pd
 from flask import Flask, request, render_template, jsonify
 from flask_pymongo import PyMongo
+from flaskext.mysql import MySQL
+#from flask_sqlalchemy import SQLAlchemy
+import mysql.connector
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -11,46 +14,55 @@ pickled_model = pickle.load(open('random_forest_model.pkl', 'rb'))
 
 app = Flask(__name__)
 
-app.config['MONGO_DBNAME'] = 'patient_database'
-app.config[
-    "MONGO_URI"] = 'mongodb://jananiim21:1234@ac-wqwdffe-shard-00-00.ckrfkgt.mongodb.net:27017,ac-wqwdffe-shard-00-01.ckrfkgt.mongodb.net:27017,ac-wqwdffe-shard-00-02.ckrfkgt.mongodb.net:27017/patient_database?ssl=true&replicaSet=atlas-vv8wvd-shard-0&authSource=admin&retryWrites=true&w=majority'
 
-mongo = PyMongo(app)  # connector
+#app.config['MONGO_DBNAME'] = 'patient_database'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://j6qbx3bgjysst4jr:mcbsdk2s27ldf37t@frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/nkw2tiuvgv6ufu1z'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Optional, but recommended to suppress SQLAlchemy deprecation warnings
+mysql_config = {
+    'host': "frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    'user': "j6qbx3bgjysst4jr",
+    'password': "mcbsdk2s27ldf37t",
+    'database': "nkw2tiuvgv6ufu1z",
+    'port': 3306,
+}
+mysql = mysql.connector.connect(**mysql_config)
+
+#db = SQLAlchemy(app)
+#app.config[
+   # "MONGO_URI"] = 'mongodb://jananiim21:1234@ac-wqwdffe-shard-00-00.ckrfkgt.mongodb.net:27017,ac-wqwdffe-shard-00-01.ckrfkgt.mongodb.net:27017,ac-wqwdffe-shard-00-02.ckrfkgt.mongodb.net:27017/patient_database?ssl=true&replicaSet=atlas-vv8wvd-shard-0&authSource=admin&retryWrites=true&w=majority'
+
+#mongo = PyMongo(app)  # connector
+#db = SQLAlchemy(app)
+
 
 
 @app.route('/')
 def home():
-    online_uses = mongo.db.users.find({"online": True})
-    return jsonify(str(online_uses))
+    #online_uses = mongo.db.users.find({"online": True})
+    return render_template('index.html')
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
 
-    db = mongo.db.patient_data_collection
+    #db = mongo.db.patient_data_collection
 
     data = request.get_json()
+    cursor = mysql.cursor()
 
-    age = data['age']
-    sex = data['sex']
-    TSH = data['TSH']
-    T3 = data['T3']
-    T4U = data['T4U']
-    FTI = data['FTI']
-    onthyroxine = data['onthyroxine']
-    queryonthyroxine = data['queryonthyroxine']
-    onantithyroidmedication = data['onantithyroidmedication']
-    sick = data['sick']
-    pregnant = data['pregnant']
-    thyroidsurgery = data['thyroidsurgery']
-    I131treatment = data['I131treatment']
-    queryhypothyroid = data['queryhypothyroid']
-    queryhyperthyroid = data['queryhyperthyroid']
-    lithium = data['lithium']
-    goitre = data['goitre']
-    tumor = data['tumor']
-    hypopituitary = data['hypopituitary']
-    psych = data['psych']
+    # Define the SQL query with placeholders
+
+
+    # Extract data values
+    new_data = (
+        data['age'], data['sex'], data['TSH'], data['T3'], data['T4U'], data['FTI'],
+        data['onthyroxine'], data['queryonthyroxine'], data['onantithyroidmedication'],
+        data['sick'], data['pregnant'], data['thyroidsurgery'], data['I131treatment'],
+        data['queryhypothyroid'], data['queryhyperthyroid'], data['lithium'],
+        data['goitre'], data['tumor'], data['hypopituitary'], data['psych']
+    )
+
+
 
     # values = ({"age": [age], "sex": [sex],
     #            "TSH": TSH, "T3": T3, "T4U": T4U, "FTI": FTI,
@@ -63,50 +75,61 @@ def predict():
     #            "hypopituitary": [hypopituitary],
     #            "psych": [psych]})
 
-    values = ({"age": age, "sex": sex,
-               "TSH": TSH, "T3": T3, "T4U": T4U, "FTI": FTI,
-               "onthyroxine": onthyroxine, "queryonthyroxine": queryonthyroxine,
-               "onantithyroidmedication": onantithyroidmedication,
-               "sick": sick, "pregnant": pregnant, "thyroidsurgery": thyroidsurgery,
-               "I131treatment": I131treatment,
-               "queryhypothyroid": queryhypothyroid, "queryhyperthyroid": queryhyperthyroid,
-               "lithium": lithium, "goitre": goitre, "tumor": tumor,
-               "hypopituitary": hypopituitary,
-               "psych": psych})
+    # values = ({"age": age, "sex": sex,
+    #            "TSH": TSH, "T3": T3, "T4U": T4U, "FTI": FTI,
+    #            "onthyroxine": onthyroxine, "queryonthyroxine": queryonthyroxine,
+    #            "onantithyroidmedication": onantithyroidmedication,
+    #            "sick": sick, "pregnant": pregnant, "thyroidsurgery": thyroidsurgery,
+    #            "I131treatment": I131treatment,
+    #            "queryhypothyroid": queryhypothyroid, "queryhyperthyroid": queryhyperthyroid,
+    #            "lithium": lithium, "goitre": goitre, "tumor": tumor,
+    #            "hypopituitary": hypopituitary,
+    #            "psych": psych})
 
-    insert_data = db.insert_one(values)
+    #insert_data = db.insert_one(values)
 
-    df_transform = pd.DataFrame.from_dict([values])
+
+    # Commit the changes to the database
+
+
+
+    df_transform = pd.DataFrame.from_dict([new_data])
 
     # print("applying transformation\n")
 
-    df_transform.age = df_transform['age'] ** (1 / 2)
+    #df_transform.age = df_transform[new_data('age')] ** (1 / 2)
+    age = new_data[0]
+
+    df_transform['age'] = age ** (1 / 2)
     print(df_transform.age)
 
-    df_transform.TSH = np.log1p(df_transform['TSH'])
+
+    TSH = new_data[2]
+    df_transform['TSH'] = np.log1p(TSH)
     # print(df_transform.TSH)
-    #
-    df_transform.T3 = df_transform['T3'] ** (1 / 2)
+    T3=new_data[3]
+    df_transform.T3 = T3 ** (1 / 2)
     # print(df_transform.T3)
+    T4U=new_data[4]
 
-    df_transform.T4U = np.log1p(df_transform['T4U'])
+    df_transform.T4U = np.log1p(T4U)
     # print(df_transform.T4U)
-
-    df_transform.FTI = df_transform['FTI'] ** (1 / 2)
-    # print(df_transform.FTI)
+    FTI=new_data[5]
+    df_transform.FTI = FTI ** (1 / 2)
+    print(FTI)
 
     df_transform.to_dict()
 
-    arr = np.array([[df_transform.age, sex,
+    arr = np.array([[df_transform.age, new_data[1],
                      df_transform.TSH, df_transform.T3, df_transform.T4U, df_transform.FTI,
-                     onthyroxine, queryonthyroxine,
-                     onantithyroidmedication,
-                     sick, pregnant, thyroidsurgery,
-                     I131treatment,
-                     queryhypothyroid, queryhyperthyroid,
-                     lithium, goitre, tumor,
-                     hypopituitary,
-                     psych]])
+                     new_data[6], new_data[7],
+                     new_data[8],
+                     new_data[9], new_data[10], new_data[11],
+                     new_data[12],
+                     new_data[13], new_data[14],
+                     new_data[15], new_data[16], new_data[17],
+                     new_data[18],
+                     new_data[19]]])
 
     # print("After transformation:\n")
     # print(arr)
@@ -120,7 +143,29 @@ def predict():
     else:
         res_Val = 'Negative'
 
+
+    insert_query = """
+    INSERT INTO thyroid_data 
+    (age, sex, TSH, T3, T4U, FTI, onthyroxine, queryonthyroxine, onantithyroidmedication, 
+    sick, pregnant, thyroidsurgery, I131treatment, queryhypothyroid, queryhyperthyroid, 
+    lithium, goitre, tumor, hypopituitary, psych, result) 
+    VALUES 
+    (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """
+    new_data = (
+        data['age'], data['sex'], data['TSH'], data['T3'], data['T4U'], data['FTI'],
+        data['onthyroxine'], data['queryonthyroxine'], data['onantithyroidmedication'],
+        data['sick'], data['pregnant'], data['thyroidsurgery'], data['I131treatment'],
+        data['queryhypothyroid'], data['queryhyperthyroid'], data['lithium'],
+        data['goitre'], data['tumor'], data['hypopituitary'], data['psych'], res_Val
+    )
+
+
+    cursor.execute(insert_query, new_data)
+    mysql.commit()
+    cursor.close()
     return jsonify(res_Val)
+
 
 
 if __name__ == '__main__':
