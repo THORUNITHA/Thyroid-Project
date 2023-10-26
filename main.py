6ca903f426abfb3f7b573c93396104ce71b6f233
JananiiM21 <148810288+JananiiM21@users.noreply.github.com>
2023-10-26 11:37:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,12 @@ import numpy as np
 import pandas as pd
 from flask import Flask, request, render_template, jsonify
 import mysql.connector
-import json
 from flask_cors import CORS
-
-import warnings
-warnings.filterwarnings('ignore')
 
 pickled_model = pickle.load(open('random_forest_model.pkl', 'rb'))
 
 app = Flask(__name__, template_folder='templates')
-CORS(app, resources={r"/api/": {"origins": ""}})
+
 
 #app.config['MONGO_DBNAME'] = 'patient_database'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://j6qbx3bgjysst4jr:mcbsdk2s27ldf37t@frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/nkw2tiuvgv6ufu1z'
@@ -25,29 +21,21 @@ mysql_config = {
     'port': 3306,
 }
 mysql = mysql.connector.connect(**mysql_config)
-version = mysql.get_server_version()
-
-#db = SQLAlchemy(app)
-#app.config[
-   # "MONGO_URI"] = 'mongodb://jananiim21:1234@ac-wqwdffe-shard-00-00.ckrfkgt.mongodb.net:27017,ac-wqwdffe-shard-00-01.ckrfkgt.mongodb.net:27017,ac-wqwdffe-shard-00-02.ckrfkgt.mongodb.net:27017/patient_database?ssl=true&replicaSet=atlas-vv8wvd-shard-0&authSource=admin&retryWrites=true&w=majority'
-
-#mongo = PyMongo(app)  # connector
-#db = SQLAlchemy(app)
 
 
 
 @app.route('/')
 def home():
     #online_uses = mongo.db.users.find({"online": True})
-    return render_template('index.html')
+    return "Thryoid Disease Detection"
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
 
     #db = mongo.db.patient_data_collection
-    headers = {"Content-Type": "application/json; charset=utf-8"}
-    data = request.json(headers=headers)
+   
+    data = request.get_json()
     cursor = mysql.cursor()
 
     # Define the SQL query with placeholders
@@ -64,40 +52,9 @@ def predict():
 
 
 
-    # values = ({"age": [age], "sex": [sex],
-    #            "TSH": TSH, "T3": T3, "T4U": T4U, "FTI": FTI,
-    #            "onthyroxine": [onthyroxine], "queryonthyroxine": [queryonthyroxine],
-    #            "onantithyroidmedication": [onantithyroidmedication],
-    #            "sick": [sick], "pregnant": [pregnant], "thyroidsurgery": [thyroidsurgery],
-    #            "I131treatment": [I131treatment],
-    #            "queryhypothyroid": [queryhypothyroid], "queryhyperthyroid": [queryhyperthyroid],
-    #            "lithium": [lithium], "goitre": [goitre], "tumor": [tumor],
-    #            "hypopituitary": [hypopituitary],
-    #            "psych": [psych]})
-
-    # values = ({"age": age, "sex": sex,
-    #            "TSH": TSH, "T3": T3, "T4U": T4U, "FTI": FTI,
-    #            "onthyroxine": onthyroxine, "queryonthyroxine": queryonthyroxine,
-    #            "onantithyroidmedication": onantithyroidmedication,
-    #            "sick": sick, "pregnant": pregnant, "thyroidsurgery": thyroidsurgery,
-    #            "I131treatment": I131treatment,
-    #            "queryhypothyroid": queryhypothyroid, "queryhyperthyroid": queryhyperthyroid,
-    #            "lithium": lithium, "goitre": goitre, "tumor": tumor,
-    #            "hypopituitary": hypopituitary,
-    #            "psych": psych})
-
-    #insert_data = db.insert_one(values)
-
-
-    # Commit the changes to the database
-
-
-
     df_transform = pd.DataFrame.from_dict([new_data])
 
-    # print("applying transformation\n")
-
-    #df_transform.age = df_transform[new_data('age')] ** (1 / 2)
+  
     age = new_data[0]
 
     df_transform['age'] = age ** (1 / 2)
@@ -166,7 +123,6 @@ def predict():
     cursor.close()
 
     return jsonify(res_Val)
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
