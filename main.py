11b80ec9208da7cea02bf3c418131faed3a03cbf
S.S.THORUNITHA <43957197+THORUNITHA@users.noreply.github.com>
2023-10-26 16:04:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 import pickle
 import numpy as np
 import pandas as pd
-from flask import Flask, request, render_template, jsonify
-import mysql.connector
+from flask import Flask, request, render_template, jsonify, send_from_directory
 from flask_cors import CORS
+import mysql.connector
 
 pickled_model = pickle.load(open('random_forest_model.pkl', 'rb'))
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 
-
-#app.config['MONGO_DBNAME'] = 'patient_database'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://j6qbx3bgjysst4jr:mcbsdk2s27ldf37t@frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/nkw2tiuvgv6ufu1z'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Optional, but recommended to suppress SQLAlchemy deprecation warnings
 mysql_config = {
     'host': "frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     'user': "j6qbx3bgjysst4jr",
@@ -22,74 +18,70 @@ mysql_config = {
 }
 mysql = mysql.connector.connect(**mysql_config)
 
-
+CORS(app)
 
 @app.route('/')
 def home():
-    #online_uses = mongo.db.users.find({"online": True})
-    return "Thryoid Disease Detection"
+    return render_template('index.html')
 
+def predict_thyroid_disease(data):
+    # Convert data to the appropriate data types
 
-@app.route('/predict', methods=['POST'])
-def predict():
-
-    #db = mongo.db.patient_data_collection
-   
     data = request.get_json()
     cursor = mysql.cursor()
 
-    # Define the SQL query with placeholders
 
+    age = data['age']
+    sex = data['sex']
+    TSH = data['TSH']
+    T3 = data['T3']
+    T4U = data['T4U']
+    FTI = data['FTI']
+    onthyroxine = data['onthyroxine']
+    queryonthyroxine = data['queryonthyroxine']
+    onantithyroidmedication = data['onantithyroidmedication']
+    sick = data['sick']
+    pregnant = data['pregnant']
+    thyroidsurgery = data['thyroidsurgery']
+    I131treatment = data['I131treatment']
+    queryhypothyroid = data['queryhypothyroid']
+    queryhyperthyroid = data['queryhyperthyroid']
+    lithium = data['lithium']
+    goitre = data['goitre']
+    tumor = data['tumor']
+    hypopituitary = data['hypopituitary']
+    psych = data['psych']
 
-    # Extract data values
-    new_data = (
-        data['age'], data['sex'], data['TSH'], data['T3'], data['T4U'], data['FTI'],
-        data['onthyroxine'], data['queryonthyroxine'], data['onantithyroidmedication'],
-        data['sick'], data['pregnant'], data['thyroidsurgery'], data['I131treatment'],
-        data['queryhypothyroid'], data['queryhyperthyroid'], data['lithium'],
-        data['goitre'], data['tumor'], data['hypopituitary'], data['psych']
-    )
+    values = {
+        "age": age, "sex": sex,
+        "TSH": TSH, "T3": T3, "T4U": T4U, "FTI": FTI,
+        "onthyroxine": onthyroxine, "queryonthyroxine": queryonthyroxine,
+        "onantithyroidmedication": onantithyroidmedication,
+        "sick": sick, "pregnant": pregnant, "thyroidsurgery": thyroidsurgery,
+        "I131treatment": I131treatment,
+        "queryhypothyroid": queryhypothyroid, "queryhyperthyroid": queryhyperthyroid,
+        "lithium": lithium, "goitre": goitre, "tumor": tumor,
+        "hypopituitary": hypopituitary,
+        "psych": psych
+    }
 
+    df_transform = pd.DataFrame.from_dict([values])
 
+    df_transform['age'] = pd.to_numeric(df_transform['age'], errors='coerce')
+    df_transform['age'] = df_transform['age'] ** (1 / 2)
+    df_transform['TSH'] = pd.to_numeric(df_transform['TSH'], errors='coerce')
+    df_transform['TSH'] = np.log1p(df_transform['TSH'])
+    df_transform['T3'] = pd.to_numeric(df_transform['T3'], errors='coerce')
+    df_transform['T3'] = df_transform['T3'] ** (1 / 2)
+    df_transform['T4U'] = pd.to_numeric(df_transform['T4U'], errors='coerce')
+    df_transform['T4U'] = np.log1p(df_transform['T4U'])
+    df_transform['FTI'] = pd.to_numeric(df_transform['FTI'], errors='coerce')
+    df_transform['FTI'] = df_transform['FTI'] ** (1 / 2)
 
-    df_transform = pd.DataFrame.from_dict([new_data])
-
-  
-    age = new_data[0]
-
-    df_transform['age'] = age ** (1 / 2)
-    print(df_transform.age)
-
-
-    TSH = new_data[2]
-    df_transform['TSH'] = np.log1p(TSH)
-    # print(df_transform.TSH)
-    T3=new_data[3]
-    df_transform.T3 = T3 ** (1 / 2)
-    # print(df_transform.T3)
-    T4U=new_data[4]
-
-    df_transform.T4U = np.log1p(T4U)
-    # print(df_transform.T4U)
-    FTI=new_data[5]
-    df_transform.FTI = FTI ** (1 / 2)
-    print(FTI)
-
-    df_transform.to_dict()
-
-    arr = np.array([[df_transform.age, new_data[1],
-                     df_transform.TSH, df_transform.T3, df_transform.T4U, df_transform.FTI,
-                     new_data[6], new_data[7],
-                     new_data[8],
-                     new_data[9], new_data[10], new_data[11],
-                     new_data[12],
-                     new_data[13], new_data[14],
-                     new_data[15], new_data[16], new_data[17],
-                     new_data[18],
-                     new_data[19]]])
-
-    # print("After transformation:\n")
-    # print(arr)
+    arr = np.array([[df_transform['age'].values[0], sex,
+                     df_transform['TSH'].values[0], df_transform['T3'].values[0], df_transform['T4U'].values[0], df_transform['FTI'].values[0],
+                     onthyroxine, queryonthyroxine, onantithyroidmedication, sick, pregnant, thyroidsurgery,
+                     I131treatment, queryhypothyroid, queryhyperthyroid, lithium, goitre, tumor, hypopituitary, psych]])
 
     pred = pickled_model.predict(arr)[0]
 
@@ -99,7 +91,6 @@ def predict():
         res_Val = "Hypothyroid"
     else:
         res_Val = 'Negative'
-
 
     insert_query = """
     INSERT INTO thyroid_data 
@@ -117,12 +108,18 @@ def predict():
         data['goitre'], data['tumor'], data['hypopituitary'], data['psych'], res_Val
     )
 
-
     cursor.execute(insert_query, new_data)
     mysql.commit()
     cursor.close()
 
-    return jsonify(res_Val)
+    return res_Val
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    result = predict_thyroid_disease(data)
+    return jsonify(result)
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, port=5000)
